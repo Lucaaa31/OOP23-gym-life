3da3066046bbbcb7f6f75b8e508c0c9ca18f0b1b
Luca <luca.camillini@studio.unibo.it>
2024-03-22 23:24:54 +0100

    Added TimerView inside MinigameView

--- src/main/java/gymlife/view/MinigameViewImpl.java
+++ src/main/java/gymlife/view/MinigameViewImpl.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
 
-public class MinigameViewImpl {
+public class MinigameViewImpl{
 
     public MinigameViewImpl(){
 
@@ -16,8 +16,8 @@ public class MinigameViewImpl {
 
         JFrame mainFrame = new JFrame("TEMPORARY");
         Dimension screenSeize = Toolkit.getDefaultToolkit().getScreenSize();
-        Integer widht = (int)screenSeize.getWidth();
-        Integer height = (int)screenSeize.getHeight();
+        int widht = (int)screenSeize.getWidth();
+        int height = (int)screenSeize.getHeight();
 
 
         mainFrame.setSize(widht, height);
@@ -26,11 +26,11 @@ public class MinigameViewImpl {
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
 
         JPanel panel1 = new JPanel();
-        panel1.setBackground(Color.BLUE); // Set background color for illustration
+        panel1.setBackground(Color.BLUE); 
         panel1.setPreferredSize(new Dimension(widht - (widht/3), height)); 
 
         JPanel panel2 = new JPanel();
-        panel2.setBackground(Color.RED); // Set background color for illustration
+        //panel2.setBackground(Color.RED); // Set background color for illustration
         panel2.setPreferredSize(new Dimension(widht/3, height)); 
 
         mainPanel.add(panel1);
@@ -42,6 +42,15 @@ public class MinigameViewImpl {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setVisible(true);
+
+        TimerView panelTimer = new TimerView(height);
+
+        panelTimer.setPreferredSize(new Dimension(widht/5, height/5));
+        
+        panel1.setLayout(new FlowLayout(FlowLayout.LEFT));
+        panel1.add(panelTimer);
+
+        panelTimer.setVisible(true);
     }
 
 
--- src/main/java/gymlife/view/TimerView.java
+++ src/main/java/gymlife/view/TimerView.java
@@ -1,31 +1,52 @@
 package gymlife.view;
 
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import gymlife.model.TimerImpl;
 
+
 public class TimerView extends JPanel {
 
-    public TimerView(final int widht, final int height ){
-        JPanel panelTimer = new JPanel();
-        panelTimer.setBackground(Color.BLACK);
-        panelTimer.setPreferredSize(new Dimension(widht/5, height/5));
+    private static JLabel label1 = new JLabel();
+    private static JLabel label2 = new JLabel();
+    private static JLabel label3 = new JLabel();
+    private static JLabel label4 = new JLabel();
+    private static JLabel label5 = new JLabel();
 
-        TimerImpl logic = new TimerImpl(10000);
-        logic.start();
 
-        while (true) {
-            System.out.print(logic.getFirstDigit());
-            System.out.print(logic.getSecondDigit());
-            System.out.print(logic.getThirdDigit());
-            System.out.println(logic.getFirstDigit());
 
-            
+    public TimerView(final int height){
 
-        }
-        
+        int n = 10;
+
+        setBackground(Color.PINK);
+
+        setLayout(new GridLayout(1, 5));
+
+        label1.setFont(new Font("Calibri", Font.ROMAN_BASELINE, height/n));
+        label2.setFont(new Font("Calibri", Font.ROMAN_BASELINE, height/n));
+        label3.setFont(new Font("Calibri", Font.ROMAN_BASELINE, height/9));
+        label4.setFont(new Font("Calibri", Font.ROMAN_BASELINE, height/n));
+        label5.setFont(new Font("Calibri", Font.ROMAN_BASELINE, height/n));
+        add(label1);
+        add(label2);
+        add(label3);
+        add(label4);
+        add(label5);
+        setVisible(true);
     }
+
+    public static void update(final int firstDigit, final int secondDigit, final int thirdDigit, final int forthDigit ){
+        label1.setText(Integer.toString(firstDigit));
+        label2.setText(Integer.toString(secondDigit));
+        label4.setText(Integer.toString(thirdDigit));
+        label5.setText(Integer.toString(forthDigit));
+    }
+
+    
 }
