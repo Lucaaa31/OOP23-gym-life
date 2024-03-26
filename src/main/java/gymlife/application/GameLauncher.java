721c09e3cb46f47026cda9b8bc469bd6952c06a9
Lucio Baiocchi <lucio.baiocchi@studio.unibo.it>
2024-03-26 11:31:09 +0100

    Removed trailing spaces

--- src/main/java/gymlife/application/GameLauncher.java
+++ src/main/java/gymlife/application/GameLauncher.java
@@ -1,9 +1,7 @@
 package gymlife.application;
 
-import gymlife.view.CharacterViewImpl;
-import gymlife.view.api.CharacterView;
 /**
- * 
+ *
  * Main Class of the Java application.
  */
 public final class GameLauncher {
@@ -17,6 +15,5 @@ public final class GameLauncher {
      * @param args No arguments passed.
      */
     public static void main(final String[] args) {
-        
     }
 }
