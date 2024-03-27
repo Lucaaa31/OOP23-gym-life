8ca17f7d8e11dc7fa0b53570bbac7e9d33c072fb
Mattia Morri <mattia.morri6@studio.unibo.it>
2024-03-27 10:45:05 +0100

    added AirplaneGameView class

--- src/main/java/gymlife/application/GameLauncher.java
+++ src/main/java/gymlife/application/GameLauncher.java
@@ -1,5 +1,6 @@
 package gymlife.application;
 
+import gymlife.view.AirplaneGameView;
 
 /**
  * 
@@ -16,6 +17,6 @@ public final class GameLauncher {
      * @param args No arguments passed.
      */
     public static void main(final String[] args) {
-
+        new AirplaneGameView();
     }
 }
--- src/main/java/gymlife/model/PlaneGameModel.java
+++ src/main/java/gymlife/model/PlaneGameModel.java
@@ -2,18 +2,48 @@ package gymlife.model;
 
 import java.util.Random;
 
-public class PlaneGameModel {
-    private int treshold;
-    private double multiplier;
+/**
+ * This class create a multiplier.
+ */
+public final class PlaneGameModel implements Runnable {
+    private static final float MAX_BOUND = 12.00f;
+    private static final int THREAD_WAIT = 5;
+    private static final float INCREMENT = 0.001f;
+    private boolean flag = true;
+    private static final Random R = new Random();
+    private final float treshold;
+    private float multiplier;
 
+    /**
+     * This is the constructor of the PlaneGameModel class.
+     */
     public PlaneGameModel() {
-        multiplier = 1.0d;
-        Random r = new Random();
-        treshold = r.nextInt(5);
+        multiplier = 1.0f;
+        treshold = (float) (Math.round((1.00 + R.nextFloat() * MAX_BOUND) * 1000.0) / 1000.0);
     }
 
-    public boolean rangeControl() {
-        return treshold == multiplier;
+    /**
+     * This method checks if the multiplier is equal to the treshold.
+     *
+     * @return true if the variable treshold is equal to the multiplier variable.
+     */
+    public boolean boundControl() {
+        final float n = (float) (Math.round(multiplier * 1000.0) / 1000.0);
+        return Float.compare(treshold, n) == 0;
+    }
+
+    @Override
+    public void run() {
+        while (flag) {
+            multiplier += INCREMENT;
+            if (boundControl()) {
+                flag = false;
+            }
+            try {
+                Thread.sleep(THREAD_WAIT);
+            } catch (InterruptedException e) {
+            }
+        }
     }
 
 }
