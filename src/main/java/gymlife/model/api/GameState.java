cec2f1d2a0ffe35cb52d64d64ff706fcedcc300b
Lucio Baiocchi <lucio.baiocchi@studio.unibo.it>
2024-04-09 16:28:32 +0200

    Removed GamestateImpl and Gamestate

--- src/main/java/gymlife/model/api/GameState.java
+++
@@ -1,9 +0,0 @@
-package gymlife.model.api;
-
-import gymlife.utility.StateType;
-
-public interface GameState {
-    void setGameState(StateType newState);
-    StateType getGameState ();
-    void setKey(char key);
-}
