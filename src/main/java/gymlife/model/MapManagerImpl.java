6001690394795ebb3789583d1ec9c065625efe5e
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-04-24 14:22:49 +0200

    Added javadoc for MapManagerImpl

--- src/main/java/gymlife/model/MapManagerImpl.java
+++ src/main/java/gymlife/model/MapManagerImpl.java
@@ -3,13 +3,17 @@ package gymlife.model;
 import gymlife.model.api.GameMap;
 import gymlife.model.api.MapManager;
 
-public class MapManagerImpl implements MapManager{
+/**
+ * Implementation of Interface MapManager, it has a private attribute {@code GameMap} that represents the current map.
+ */
+public class MapManagerImpl implements MapManager {
 
     private GameMap currentMap;
 
     public MapManagerImpl(final GameMap initialMap) {
         this.currentMap = initialMap;
     }
+
     @Override
     public void changeMap(GameMap newMap) {
         this.currentMap = newMap;
