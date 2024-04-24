130ab13d347d7541621835bd048083d2a7dcd67d
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-04-24 11:28:17 +0200

    Implemented methods and attributes of class MapManagerImpl.

--- src/main/java/gymlife/model/MapManagerImpl.java
+++ src/main/java/gymlife/model/MapManagerImpl.java
@@ -5,13 +5,18 @@ import gymlife.model.api.MapManager;
 
 public class MapManagerImpl implements MapManager{
 
+    private GameMap currentMap;
+
+    public MapManagerImpl(final GameMap initialMap) {
+        this.currentMap = initialMap;
+    }
     @Override
     public void changeMap(GameMap newMap) {
-
+        this.currentMap = newMap;
     }
 
     @Override
     public GameMap getCurrentMap() {
-        return null;
+        return currentMap;
     }
 }
