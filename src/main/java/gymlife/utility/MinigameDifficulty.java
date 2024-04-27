859d96547b91469acebfac9426378443291cfc49
Luca Camillini <luca.camillini@studio.unibo.it>
2024-04-27 16:12:47 +0200

    Modified Enum MinigameDifficulty

--- src/main/java/gymlife/utility/MinigameDifficulty.java
+++ src/main/java/gymlife/utility/MinigameDifficulty.java
@@ -1,12 +1,56 @@
 package gymlife.utility;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public enum MinigameDifficulty {
-    EASY(),
-    MEDIUM(),
-    HARD();
+    EASY(
+            2,
+            3,
+            7000,
+            new HashSet<Character>(Set.of('Q', 'W', 'E', 'R', 'T', 'Y'))
+    ),
+    MEDIUM(
+            3,
+            1,
+            5000,
+            new HashSet<Character>(Set.of('Q', 'W', 'E', 'R', 'T', 'Y',
+                    'A', 'S', 'D', 'F', 'Z', 'X', 'C'))
+    ),
+    HARD(
+            5,
+            0,
+            3000,
+            new HashSet<Character>(Set.of('Q', 'W', 'E', 'R', 'T', 'Y',
+                    'A', 'S', 'D', 'F', 'Z', 'X', 'C', 'U', 'I',
+                    'O', 'P', 'G', 'H', 'J', 'K', 'L', 'B', 'N', 'M'))
+    );
 
+    private Set<Character> keys;
+    private int nReps;
+    private int nMistakes;
+    private int reactionTime;
 
-    private MinigameDifficulty() {
+    MinigameDifficulty(final int nReps, final int nMistakes, final int reactionTime, final Set<Character> keys) {
+        this.keys = keys;
+        this.nReps = nReps;
+        this.nMistakes = nMistakes;
+        this.reactionTime = reactionTime;
+    }
 
+    public Set<Character> getKeys() {
+        return keys;
+    }
+
+    public int getnReps() {
+        return nReps;
+    }
+
+    public int getnMistakes() {
+        return nMistakes;
+    }
+
+    public int getReactionTime() {
+        return reactionTime;
     }
 }
