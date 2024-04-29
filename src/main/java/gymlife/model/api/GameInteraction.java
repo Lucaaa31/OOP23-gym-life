cb345fbd8a999fa1f35a2682f83911b031d9d40f
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-04-29 15:18:30 +0200

    Modified interface GameInteraction to make it into a functional interface so it can be used for lambdas.

--- src/main/java/gymlife/model/api/GameInteraction.java
+++ src/main/java/gymlife/model/api/GameInteraction.java
@@ -3,16 +3,8 @@ package gymlife.model.api;
 /**
  * An interface for Interactions in the game.
  * @param <T> Type of the game element on which the interaction has to happen.
- * @param <R> Type of the eventual input Object required by the interaction.
  */
-public interface GameInteraction<T, R> {
-    /**
-     * Method to actually do the interaction. When implemented it should call a method of gameElementToChange.
-     * @param gameElementToChange the Game element on which to do the interaction.
-     * @param input input for the interaction.
-     */
-    void interactWithInput(T gameElementToChange, R input);
-
+public interface GameInteraction<T> {
     /**
      * Method to actually do the interaction. When implemented it should call a method of gameElementToChange.
      * @param gameElementToChange the Game element on which to do the interaction.
