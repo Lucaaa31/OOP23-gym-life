c01cf6417ddf80d41425ae263628fad8cdfe6a49
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-04-30 11:20:27 +0200

    added a MapManager to the controller, also added all useful methods to manipulate it.

--- src/main/java/gymlife/controller/Controller.java
+++ src/main/java/gymlife/controller/Controller.java
@@ -1,5 +1,35 @@
 package gymlife.controller;
 
-public class Controller {
+import gymlife.model.GameMapImpl;
+import gymlife.model.MapManagerImpl;
+import gymlife.model.api.GameMap;
+import gymlife.model.api.MapManager;
+
+/**
+ * the controller (to change)
+ */
+public final class Controller {
+
+    private final MapManager mapManager;
+
+    public Controller() {
+        this.mapManager = new MapManagerImpl(GameMapImpl.HOUSE_MAP);
+    }
+
+    public void goToGym() {
+        mapManager.changeMap(GameMapImpl.GYM_MAP);
+    }
+
+    public void goToShop() {
+        mapManager.changeMap(GameMapImpl.SHOP_MAP);
+    }
+
+    public void goToHouse() {
+        mapManager.changeMap(GameMapImpl.HOUSE_MAP);
+    }
+
+    public GameMap getCurrentMap() {
+        return mapManager.getCurrentMap();
+    }
 
 }
