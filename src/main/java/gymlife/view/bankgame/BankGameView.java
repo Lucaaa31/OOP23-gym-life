4fa3df2e8bb07570139a118e50cfe10f30103aff
Mattia Morri <mattia.morri6@studio.unibo.it>
2024-05-07 11:24:46 +0200

    Added button and made it resizable.

--- src/main/java/gymlife/view/bankgame/BankGameView.java
+++ src/main/java/gymlife/view/bankgame/BankGameView.java
@@ -1,5 +1,6 @@
 package gymlife.view.bankgame;
 
+import javax.swing.JButton;
 import javax.swing.JLayeredPane;
 import gymlife.controller.BankGameController;
 import java.awt.Dimension;
@@ -22,35 +23,45 @@ public final class BankGameView extends JLayeredPane {
      * This method sets the dimensions of the plane image and the sky image,
      * moreover it sets the images' layering.
      */
-    public BankGameView() {
+    public BankGameView(BankGameController controller) {
         final TextLabelView numberLabel = new MultiplierGameView();
         final ImageLabelView planeLayer = new AirplaneGameView();
         final ImageLabelView skyLayer = new SkyGameView();
+        final JButton button = new JButton();
 
         this.add(skyLayer, JLayeredPane.DEFAULT_LAYER);
         this.add(planeLayer, JLayeredPane.PALETTE_LAYER);
         this.add(numberLabel, JLayeredPane.MODAL_LAYER);
+        this.add(button, JLayeredPane.MODAL_LAYER);
 
         this.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(final ComponentEvent e) {
-                setLayersNewSize(skyLayer, planeLayer, numberLabel);
+                setLayersNewSize(skyLayer, planeLayer, numberLabel, button);
             }
         }); 
+        
 
-        //Test
+        /*
+         *  //Test
         if (numberLabel instanceof MultiplierGameView mpgv){
             mpgv.updateText(0.2534f);
         }
+         */
+       
 
         this.setVisible(true);
     }
+    
 
     private void setLayersNewSize(final ImageLabelView skyLabel, final ImageLabelView planeLabel,
-            final TextLabelView numberLabel) {
+            final TextLabelView numberLabel, final JButton button) {
         final Dimension newSize = this.getSize();
         skyLabel.setBounds(0, 0, newSize.width, newSize.height);
         skyLabel.reload();
+        button.setBounds(newSize.width / 35,
+        newSize.height / 2, newSize.height / 4,
+        newSize.height / 11);
         planeLabel.setBounds(newSize.width / 4,
                 newSize.height / 4, newSize.height / 2,
                 newSize.height / 2);
@@ -61,3 +72,4 @@ public final class BankGameView extends JLayeredPane {
         numberLabel.reload();
     }
 }
+
