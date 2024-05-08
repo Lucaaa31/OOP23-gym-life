e6d0dbf43a335d300fd5b62ca99c04ea15b95beb
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-05-08 14:15:07 +0200

    Added 3 positions to MapConstants to indicate the positions in which the player spawns when entering said map.

--- src/main/java/gymlife/utility/MapConstants.java
+++ src/main/java/gymlife/utility/MapConstants.java
@@ -16,6 +16,18 @@ public final class MapConstants {
      * Standard path for Map files.
      */
     public static final String MAP_FILES_PATH = "maps/";
+    /**
+     * The position on which the player must spawn when going into the House map.
+     */
+    public static final Position HOUSE_INITIAL_POSITION = new Position(6, 2);
+    /**
+     * The position on which the player must spawn when going into the Shop map.
+     */
+    public static final Position SHOP_INITIAL_POSITION = new Position(6, 4);
+    /**
+     * The position on which the player must spawn when going into the Gym map.
+     */
+    public static final Position GYM_INITIAL_POSITION = new Position(1, 3);
 
     private MapConstants() {
 
