7598318318f5e2a9c1d351e2f1a659fe5b9ce86a
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-05-09 13:53:32 +0200

    Modified GameMapView, so it now can display both the player and the map, using a JLayeredPane.

--- src/main/java/gymlife/view/GameMapView.java
+++ src/main/java/gymlife/view/GameMapView.java
@@ -1,11 +1,14 @@
 package gymlife.view;
 
 import gymlife.controller.api.Controller;
+import gymlife.utility.Directions;
 import gymlife.utility.MapConstants;
 import gymlife.utility.Position;
 
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serial;
@@ -22,7 +25,7 @@ public final class GameMapView extends JFrame {
     @Serial
     private static final long serialVersionUID = -3544425405075144844L;
     private final transient Controller controller;
-    private transient Map<Position, JLayeredPane> cells = new HashMap<>();
+    private transient Map<Position, JLabel> cells = new HashMap<>();
 
     /**
      * Constructor for the GameMapView. it requires an external controller, given by the MainView.
@@ -32,30 +35,42 @@ public final class GameMapView extends JFrame {
         this.controller = controller;
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(800, 600);
-        JPanel mainPanel = new JPanel();
-        this.getContentPane().add(mainPanel);
-        mainPanel.setBackground(new Color(50,50,50));
+        JLayeredPane mainPanel = new JLayeredPane();
         JPanel mapPanel = new JPanel(new GridLayout(MapConstants.MAP_Y_DIM, MapConstants.MAP_X_DIM));
+        mapPanel.setSize(800, 600);
         mainPanel.add(mapPanel);
+        mainPanel.setLayer(mapPanel, JLayeredPane.DEFAULT_LAYER);
+        String p = "images/character/level1_down.png";
+        ImageIcon img =new ImageIcon(new ImageIcon(ClassLoader.getSystemResource(p)).getImage().getScaledInstance(this.getWidth()/8, this.getHeight()/6, Image.SCALE_SMOOTH));
+        JLabel characterLabel = new JLabel(img);
+        characterLabel.setSize(100, 100);
+        mainPanel.add(characterLabel);
+        mainPanel.setLayer(characterLabel, JLayeredPane.PALETTE_LAYER);
+
+
 
 
         for (int y = 0; y < MapConstants.MAP_Y_DIM; y++) {
             for (int x = 0; x < MapConstants.MAP_X_DIM; x++) {
                 final Position pos = new Position(x, y);
-
                 final String path = "images/cells/" + controller.getCurrentMap().getCellAtCoord(pos).getName() + ".png";
                 JLabel lb = new JLabel();
                 ImageIcon imageIcon = new ImageIcon(new ImageIcon(ClassLoader.getSystemResource(path)).getImage().getScaledInstance(this.getWidth()/8, this.getHeight()/6, Image.SCALE_SMOOTH));
                 lb.setIcon(imageIcon);
                 mapPanel.add(lb);
+                cells.put(pos, lb);
             }
         }
 
 
-
+        this.getContentPane().add(mainPanel);
 
         this.setVisible(true);
+
+
+        moveCharacter(characterLabel);
     }
+
     @Serial
     private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
@@ -71,14 +86,8 @@ public final class GameMapView extends JFrame {
     }
 
 
-
-    /**
-     * Method to get a JLayered pane from a position.
-     * @param pos position.
-     * @return returns the JLayered pane.
-     */
-    public JLayeredPane getCellFromPosition(final Position pos) {
-        return cells.get(pos);
+    private void moveCharacter(JLabel jl) {
+        Point locationToGo = new Point(cells.get(controller.getCharacterPos()).getX(),cells.get(controller.getCharacterPos()).getY());
+        jl.setLocation(locationToGo);
     }
-
 }
