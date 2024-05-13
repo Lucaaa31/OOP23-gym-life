c270605c8e1197ac13485e0ea8d4cfa96ba430d4
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-05-13 08:48:38 +0200

    Added methods and listeners to change image when the mouse cursor is over the button.

--- src/main/java/gymlife/view/FastTravelView.java
+++ src/main/java/gymlife/view/FastTravelView.java
@@ -1,12 +1,14 @@
 package gymlife.view;
 
 import gymlife.controller.api.Controller;
+import gymlife.utility.MapConstants;
 
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
-import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.Serial;
 
 /**
@@ -17,6 +19,8 @@ public final class FastTravelView extends JFrame {
     @Serial
     private static final long serialVersionUID = -5960084209507548196L;
     private final transient Controller controller;
+    private final int size;
+    private final JLabel mapLabel;
 
     /**
      * Constructor for the Interface.
@@ -24,18 +28,90 @@ public final class FastTravelView extends JFrame {
      */
     public FastTravelView(final Controller controller) {
         this.controller = controller;
+        this.size = 100;
+        this.setSize(MapConstants.MAP_X_DIM * size, MapConstants.MAP_Y_DIM * size);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         final JPanel mainPanel = new JPanel(new BorderLayout());
         final JPanel mapPanel = new JPanel(new BorderLayout());
+        mapPanel.setBackground(new Color(4, 35, 0));
         final JPanel buttonsPanel = new JPanel(new BorderLayout());
+        final JButton gymButton = new JButton("gym");
+        final JButton houseButton = new JButton("house");
+        final JButton shopButton = new JButton("shop");
+        buttonsPanel.add(gymButton, BorderLayout.EAST);
+        buttonsPanel.add(houseButton, BorderLayout.CENTER);
+        buttonsPanel.add(shopButton, BorderLayout.WEST);
+
+        final MouseListener ml = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                final String loc = ((JButton) e.getSource()).getText();
+                if (!loc.equals(getMap())){
+                    showWay(loc);
+                }
+
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                changeLocation();
+
+            }
+        };
+
+        ActionListener al = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final String loc = ((JButton) e.getSource()).getText();
+
+                changeLocation();
+            }
+        };
+
+        gymButton.addMouseListener(ml);
+        houseButton.addMouseListener(ml);
+        shopButton.addMouseListener(ml);
+
 
         this.getContentPane().add(mainPanel);
-        mainPanel.add(mapPanel, BorderLayout.NORTH);
+        mainPanel.add(mapPanel, BorderLayout.CENTER);
         mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
-        final String fileName = "current_" + getMap() + "_map.png";
-        mapPanel.add(new JLabel(new ImageIcon(ClassLoader.getSystemResource(
-                "images/fastTravelMap/" + fileName))));
+        this.mapLabel = new JLabel();
+        changeLocation();
+        mapPanel.add(mapLabel);
         this.setVisible(true);
     }
+
+    private void showWay(final String whereToGo) {
+        final String fileName = getMap() + "_map_to_" + whereToGo + "_map.png";
+        final ImageIcon img = new ImageIcon(ClassLoader.getSystemResource(
+                "images/fastTravelMap/" + fileName));
+        mapLabel.setIcon(img);
+    }
+
+    private void changeLocation() {
+        final String fileName = "current_" + getMap() + "_map.png";
+        final ImageIcon img = new ImageIcon(ClassLoader.getSystemResource(
+                "images/fastTravelMap/" + fileName));
+        mapLabel.setIcon(img);
+    }
+
 
     private String getMap() {
         return controller.getCurrentMap().getName();
