405db0c5b350b593f6bf0d44f032ff58dcd2bd95
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-05-17 15:45:12 +0200

    Added new class SleepView for when the character sleeps.

---
+++ src/main/java/gymlife/view/SleepView.java
@@ -0,0 +1,15 @@
+package gymlife.view;
+
+import gymlife.view.api.GamePanel;
+
+public class SleepView extends GamePanel {
+    @Override
+    public void resizeComponents() {
+
+    }
+
+    @Override
+    public String getPanelName() {
+        return "";
+    }
+}
