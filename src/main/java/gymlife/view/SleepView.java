b2782238746eb3a9fe765fdb320df075488738d5
AndreaDeCarli <andrea.decarli2@studio.unibo.it>
2024-05-17 17:23:44 +0200

    Added methods and attributes to SleepView.

--- src/main/java/gymlife/view/SleepView.java
+++ src/main/java/gymlife/view/SleepView.java
@@ -1,8 +1,63 @@
 package gymlife.view;
 
+import gymlife.controller.api.Controller;
+import gymlife.utility.ScenariosType;
 import gymlife.view.api.GamePanel;
 
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
 public class SleepView extends GamePanel {
+
+    private final JLabel imageLabel;
+    private final Controller controller;
+    private final DimensionGetter dimensionGetter;
+
+    public SleepView(final Controller controller, final DimensionGetter dimensionGetter) {
+        this.imageLabel = new JLabel();
+        this.controller = controller;
+        this.dimensionGetter = dimensionGetter;
+        this.setBackground(Color.BLACK);
+        this.add(imageLabel);
+        imageLabel.setIcon(loadResizedImage(0));
+
+        this.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                imageLabel.setIcon(loadResizedImage(0));
+                startAnimation();
+            }
+        });
+    }
+
+    private void startAnimation() {
+        new Thread(() -> {
+            for (int i = 0; i < 4; i++) {
+                try {
+                    imageLabel.setIcon(loadResizedImage(i));
+                    Thread.sleep(500);
+                } catch (InterruptedException ignored) {
+                }
+            }
+            controller.changeScenario(ScenariosType.INDOOR_MAP);
+            imageLabel.setIcon(loadResizedImage(0));
+            this.transferFocus();
+        }).start();
+    }
+
+    private ImageIcon loadResizedImage(final int state) {
+        final String path = "images/sleeping/sprite_" + state + ".png";
+        final Image imgToResize = new ImageIcon(ClassLoader.getSystemResource(path)).getImage();
+        return new ImageIcon(imgToResize.getScaledInstance(
+                dimensionGetter.getScenarioDimension().width,
+                dimensionGetter.getScenarioDimension().height,
+                Image.SCALE_SMOOTH));
+    }
+
     @Override
     public void resizeComponents() {
 
@@ -10,6 +65,7 @@ public class SleepView extends GamePanel {
 
     @Override
     public String getPanelName() {
-        return "";
+        return "sleep";
     }
 }
+
