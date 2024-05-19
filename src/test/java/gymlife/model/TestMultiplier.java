40f06770f3782e57be8c282afa90a8c219ae9645
Mattia Morri <mattia.morri6@studio.unibo.it>
2024-05-19 02:34:43 +0200

    Fixed thread synchronization issue.

--- src/test/java/gymlife/model/TestMultiplier.java
+++ src/test/java/gymlife/model/TestMultiplier.java
@@ -1,9 +1,15 @@
 package gymlife.test;
 
+import gymlife.controller.ControllerImpl;
 import gymlife.model.PlaneGameModel;
+import gymlife.model.statistics.api.SynchronizerModel;
+import gymlife.view.bankgame.BankGameView;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.ModuleLayer.Controller;
 
 /**
  * This test check if the value of treshold and multiplierShort.
@@ -12,9 +18,26 @@ class TestMultiplier {
 
     @Test
     void equal() throws InterruptedException {
-        final PlaneGameModel number = new PlaneGameModel();
+        final ControllerImpl controller = new ControllerImpl();
+        final PlaneGameModel number = new PlaneGameModel(controller.getSync1(), controller.getSync2());
+        final BankGameView view = new BankGameView(controller);
         final float money = 20;
-        number.runMultiplier(money);
+        
+            Thread modelThread = new Thread(() -> {
+            number.runMultiplier(money);
+            });
+
+            modelThread.start();
+
+
+            controller.getSync2().signal();
+            view.showsMulti(controller);
+
+            modelThread.join();
+            //Thread.sleep(300);
+
+            System.out.println(number.getMultiplierShort());
+            System.out.println(number.getTreshold());
 
         assertEquals(number.getTreshold(), number.getMultiplierShort());
     }
